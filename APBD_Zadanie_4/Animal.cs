8ec6932253fde547a46f91004f0a13f9a1638d74
Jakub <kuba.biniek01@gmail.com>
2024-04-08 14:24:32 +0200

    Animal definition

---
+++ APBD_Zadanie_4/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_Zadanie_4
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Weight { get; set; }
+
+        public string Color { get; set; }
+    }
+}
