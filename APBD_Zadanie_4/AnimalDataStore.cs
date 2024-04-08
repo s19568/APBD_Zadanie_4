fa0c0eeae6a89ae24bbf6e2fd0c9b65a8c2bb105
Jakub <kuba.biniek01@gmail.com>
2024-04-08 14:25:14 +0200

    Animal data store implementation

---
+++ APBD_Zadanie_4/AnimalDataStore.cs
@@ -0,0 +1,43 @@
+﻿namespace APBD_Zadanie_4
+{
+    public class AnimalsDataStore
+    {
+
+        public List<Animal> Animals { get; set; }
+
+        public static AnimalsDataStore Current { get; } = new AnimalsDataStore();
+
+        public AnimalsDataStore()
+        {
+            Animals = new List<Animal>()
+            {
+                 new Animal
+                 {
+                   Id=1 ,
+                   Name ="Fifek",
+                   Category="Dog",
+                   Color="White",
+                   Weight=12,
+                 },
+                 new Animal
+                 {
+                     Id=2 ,
+                     Name ="Miauczek",
+                     Category="Cat",
+                     Color="Orange",
+                     Weight=7,
+
+                 },
+                 new Animal
+                 {
+                     Id=3 ,
+                     Name ="Owca",
+                     Category="Sheep",
+                     Color="Black",
+                     Weight=9,
+
+                 },
+            };
+        }
+    }
+}
